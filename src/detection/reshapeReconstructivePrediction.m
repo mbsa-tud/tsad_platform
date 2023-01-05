2783b8dfe20d58876ed0e4d7ad8c0392bff089e1
Adrian Wolf <adrian-wolf1@t-online.de>
2023-01-05 12:21:09 +0100

    data reshaping bugfix

--- src/detection/reshapeReconstructivePrediction.m
+++ src/detection/reshapeReconstructivePrediction.m
@@ -11,7 +11,11 @@ if iscell(prediction)
         isMultivariate = false;
     end
 else
-    isMultivariate = false;
+    if size(prediction, 2) > windowSize
+        isMultivariate = true;
+    else
+        isMultivariate = false;
+    end
 end
 
 if ~isMultivariate
@@ -34,30 +38,60 @@ if ~isMultivariate
     end
     pred = b;
 else
-    pred = zeros((size(prediction, 1) - windowSize), size(prediction{1, 1}, 1));
-    
-    for h = 1:size(prediction{1, 1}, 1)
-        data = zeros(size(prediction, 1), size(prediction{1, 1}, 2));
-        for j = 1:size(prediction, 1)
-            data(j, :) = prediction{j, 1}(h, :);
-        end
-    
-        c = [];
-        if iscell(data)
-            for i = 1:size(data, 1)
-                c(:, i) = data{i, :};
+    if iscell(prediction)
+        numChannels = size(prediction{1, 1}, 1);
+        pred = zeros((size(prediction, 1) - windowSize), numChannels);
+        
+        for h = 1:numChannels
+            data = zeros(size(prediction, 1), size(prediction{1, 1}, 2));
+            for j = 1:size(prediction, 1)
+                data(j, :) = prediction{j, 1}(h, :);
             end
-        else
-            for i = 1:size(data, 1)
-                c(:, i) = data(i, :);
+        
+            c = [];
+            if iscell(data)
+                for i = 1:size(data, 1)
+                    c(:, i) = data{i, :};
+                end
+            else
+                for i = 1:size(data, 1)
+                    c(:, i) = data(i, :);
+                end
             end
+            b = [];
+            c = flip(c);
+            for i = 1:(size(c, 2) - windowSize)
+                b(i, :) = median(diag(c(:, i:(i + windowSize - 1))));
+            end
+            pred(:, h) = b;
         end
-        b = [];
-        c = flip(c);
-        for i = 1:(size(c, 2) - windowSize)
-            b(i, :) = median(diag(c(:, i:(i + windowSize - 1))));
+    else
+        numChannels = round(size(prediction, 2) / windowSize);
+        pred = zeros((size(prediction, 1) - windowSize), numChannels);
+        
+        for h = 1:numChannels
+            data = zeros(size(prediction, 1), windowSize);
+            for j = 1:size(prediction, 1)
+                data(j, :) = prediction(j, ((h - 1) * windowSize + 1):((h - 1) * windowSize + windowSize));
+            end
+        
+            c = [];
+            if iscell(data)
+                for i = 1:size(data, 1)
+                    c(:, i) = data{i, :};
+                end
+            else
+                for i = 1:size(data, 1)
+                    c(:, i) = data(i, :);
+                end
+            end
+            b = [];
+            c = flip(c);
+            for i = 1:(size(c, 2) - windowSize)
+                b(i, :) = median(diag(c(:, i:(i + windowSize - 1))));
+            end
+            pred(:, h) = b;
         end
-        pred(:, h) = b;
     end
 end
 end
