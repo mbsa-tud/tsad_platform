d1a020ddc2343f5901fb16812e40468d41c2a969
Adrian Wolf <adrian-wolf1@t-online.de>
2023-02-28 16:44:10 +0100

    forgot simething

--- src/detection/mergeSequences.m
+++ src/detection/mergeSequences.m
@@ -1,8 +1,10 @@
 function mergedData = mergeSequences(data, windowSize)
 reshapedData = flip(data');
 
-mergedData = zeros(size(data, 1), 1);
-for i = 1:(size(reshapedData, 2) - windowSize)
+newSequenceLength = size(reshapedData, 2) - windowSize;
+
+mergedData = zeros(newSequenceLength, 1);
+for i = 1:newSequenceLength
     mergedData(i, 1) = median(diag(reshapedData(:, i:(i + windowSize - 1))));
 end
 end
