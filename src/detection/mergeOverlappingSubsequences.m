d36c176b61b5d2fbee48790fc7f9619989598071
unknown <adrian-wolf1@t-online.de>
2023-02-28 14:44:18 +0100

    update manual

--- src/detection/mergeOverlappingSubsequences.m
+++ src/detection/mergeOverlappingSubsequences.m
@@ -4,36 +4,32 @@ function reshapedPrediction = mergeOverlappingSubsequences(options, prediction)
 % Calculates the median anomaly score for all overlapping subsequences for
 % the reconstructive DL models
 
-switch options.model
-    case "Your model"
-    otherwise
-        windowSize = options.hyperparameters.data.windowSize.value;
-        dataType = options.dataType;
+windowSize = options.hyperparameters.data.windowSize.value;
+dataType = options.dataType;
 
-        if dataType == 1
-            numChannels = round(size(prediction, 2) / windowSize);
-            reshapedPrediction = zeros((size(prediction, 1) - windowSize), numChannels);
-            
-            for h = 1:numChannels
-                data = zeros(size(prediction, 1), windowSize);
-                for j = 1:size(prediction, 1)
-                    data(j, :) = prediction(j, ((h - 1) * windowSize + 1):((h - 1) * windowSize + windowSize));
-                end
-            
-                reshapedPrediction(:, h) = mergeSequences(data, windowSize);
-            end
-        elseif dataType == 2
-            numChannels = size(prediction{1, 1}, 1);
-            reshapedPrediction = zeros((size(prediction, 1) - windowSize), numChannels);
-            
-            for h = 1:numChannels
-                data = zeros(size(prediction, 1), size(prediction{1, 1}, 2));
-                for j = 1:size(prediction, 1)
-                    data(j, :) = prediction{j, 1}(h, :);
-                end
-            
-                reshapedPrediction(:, h) = mergeSequences(data, windowSize);
-            end
+if dataType == 1
+    numChannels = round(size(prediction, 2) / windowSize);
+    reshapedPrediction = zeros((size(prediction, 1) - windowSize), numChannels);
+    
+    for h = 1:numChannels
+        data = zeros(size(prediction, 1), windowSize);
+        for j = 1:size(prediction, 1)
+            data(j, :) = prediction(j, ((h - 1) * windowSize + 1):((h - 1) * windowSize + windowSize));
         end
+    
+        reshapedPrediction(:, h) = mergeSequences(data, windowSize);
+    end
+elseif dataType == 2
+    numChannels = size(prediction{1, 1}, 1);
+    reshapedPrediction = zeros((size(prediction, 1) - windowSize), numChannels);
+    
+    for h = 1:numChannels
+        data = zeros(size(prediction, 1), size(prediction{1, 1}, 2));
+        for j = 1:size(prediction, 1)
+            data(j, :) = prediction{j, 1}(h, :);
+        end
+    
+        reshapedPrediction(:, h) = mergeSequences(data, windowSize);
+    end
 end
 end
