0ec282ac0d3a5aa723efd4f6dbb4c1a34027e714
Adrian Wolf <adrian-wolf1@t-online.de>
2023-04-17 14:40:48 +0200

    cleaner auto run function

--- src/auto_run/autoRunWrapper.m
+++ src/auto_run/autoRunWrapper.m
@@ -1,4 +1,6 @@
-function finalTable =  autoRunWrapper(datasetPath, models, preprocMethod, ratioTestVal, thresholds, dynamicThresholdSettings, trainingPlots, parallelEnabled)
+function finalTable = autoRunWrapper(datasetPath, models, preprocMethod, ...
+    ratioTestVal, thresholds, dynamicThresholdSettings, trainingPlots, parallelEnabled, ...
+    augmentationEnabled, augmentationMode, augmentationIntensity, augmentedTrainingEnabled)
 %EVALUATEALLFORDATASET Main function for training and testing all specified
 %models on an entire dataset
 
@@ -13,26 +15,9 @@ scoreNames.Properties.VariableNames = "Metric";
 % Create folders for results
 % Structure: Dataset_Sweep_Results -> datasetName -> thresholdName -> all_results, 
 %                                                                     Average_Scores.csv
-datasetOutputFolder = fullfile(pwd, 'Auto_Run_Results');
-if ~exist(datasetOutputFolder, 'dir')
-    mkdir(datasetOutputFolder);
-end
 
 datasetPath_split = strsplit(datasetPath, filesep);
 datasetName = datasetPath_split{end};
-datasetOutputFolder = fullfile(datasetOutputFolder, datasetName);
-if ~exist(datasetOutputFolder, 'dir')
-    mkdir(datasetOutputFolder);
-end
-
-thresholdSubfolders = strings(length(thresholds), 1);
-
-for thr_idx = 1:length(thresholds)
-    thresholdSubfolders(thr_idx) = fullfile(datasetOutputFolder, thresholds(thr_idx));
-    if ~exist(thresholdSubfolders(thr_idx), 'dir')
-        mkdir(thresholdSubfolders(thr_idx));
-    end
-end
 
 fprintf('Selected dataset: %s\n', datasetName);
 
@@ -57,14 +42,20 @@ end
 
 
 % Initialize table for evaluation results
-scoreMatrix = cell(length(thresholds), 1);
+allScores = cell(length(thresholds), 1);
 
 allTestFiles = [];
 
 % Get all model names
 finalTableVariableNames = "Metric";
 for model_idx = 1:length(models)
-    finalTableVariableNames = [finalTableVariableNames models(model_idx).modelOptions.label];
+    if augmentationEnabled
+        finalTableVariableNames = [finalTableVariableNames, ...
+            sprintf("%s", models(model_idx).modelOptions.label), ...
+            sprintf("%s  (augmented data)", models(model_idx).modelOptions.label)];
+    else
+        finalTableVariableNames = [finalTableVariableNames, models(model_idx).modelOptions.label];
+    end
 end
 
 
@@ -75,49 +66,69 @@ for dataset_idx = 1:length(indices)
     if isMultiEntity
         fprintf('\n\nEvaluating subset %d/%d \n\n', dataset_idx, length(indices));
         fprintf('%s\n\n', subFolders(indices(dataset_idx)).name);
-        dataPath = fullfile(datasetPath, subFolders(indices(dataset_idx)).name);
+        subsetPath = fullfile(datasetPath, subFolders(indices(dataset_idx)).name);
     else
-        dataPath = datasetPath;
+        subsetPath = datasetPath;
     end
     
     % Run evaluation
-    [tmpScores, testFileNames] = trainAndEvaluateAllModels(dataPath, models, ...
+    [subsetScores, testFileNames] = trainAndEvaluateAllModels(subsetPath, models, ...
         preprocMethod, ratioTestVal, thresholds, dynamicThresholdSettings, ...
-        trainingPlots, parallelEnabled, false, [], [], []);
+        trainingPlots, parallelEnabled, augmentationEnabled, augmentationMode, ...
+        augmentationIntensity, augmentedTrainingEnabled);
 
-    allTestFiles = [allTestFiles testFileNames];
+    allTestFiles = [allTestFiles; testFileNames];
 
     for thr_idx = 1:length(thresholds)
-        scoreMatrix{thr_idx, 1} = [scoreMatrix{thr_idx, 1}; tmpScores{thr_idx, 1}];
+        allScores{thr_idx, 1} = [allScores{thr_idx, 1}; subsetScores{thr_idx, 1}];
     end
     fprintf('\n ----------------------------- \n');
 end
 
+datasetOutputFolder = fullfile(pwd, 'Auto_Run_Results');
+if ~exist(datasetOutputFolder, 'dir')
+    mkdir(datasetOutputFolder);
+end
 
+datasetPath_split = strsplit(datasetPath, filesep);
+datasetName = datasetPath_split{end};
+datasetOutputFolder = fullfile(datasetOutputFolder, datasetName);
+if ~exist(datasetOutputFolder, 'dir')
+    mkdir(datasetOutputFolder);
+end
+
+thresholdSubfolders = strings(length(thresholds), 1);
+
+for thr_idx = 1:length(thresholds)
+    thresholdSubfolders(thr_idx) = fullfile(datasetOutputFolder, getThresholdLabels(thresholds(thr_idx)));
+    if ~exist(thresholdSubfolders(thr_idx), 'dir')
+        mkdir(thresholdSubfolders(thr_idx));
+    end
+end
 
 % Score calculations and saving of results
 fprintf('\nCalculating max, min, average and standard deviation of scores\n\n')
-for thr_idx = 1:length(scoreMatrix)
-    scoreMatrix_tmp = scoreMatrix{thr_idx, 1};
+for thr_idx = 1:length(allScores)
+    scoreMatrix_tmp = allScores{thr_idx, 1};
 
-    numOfTestingFiles = length(scoreMatrix_tmp);
+    numTestedFiles = length(scoreMatrix_tmp);
     
     % Calc average scores
     avgScores = calcAverageScores(scoreMatrix_tmp);
     
     outputFolder = thresholdSubfolders(thr_idx);
 
-    all_results_folder = fullfile(outputFolder, 'all_results');
-    if ~exist(all_results_folder, 'dir')
-        mkdir(all_results_folder);
+    allResultsFolder = fullfile(outputFolder, 'all_results');
+    if ~exist(allResultsFolder, 'dir')
+        mkdir(allResultsFolder);
     end
     
-    for data_idx = 1:numOfTestingFiles
-        all_results_filename = fullfile(all_results_folder, sprintf('%s_%s.csv', allTestFiles(data_idx), datestr(now,'mm-dd-yyyy_HH-MM')));
+    for data_idx = 1:numTestedFiles
+        allResultsFileName = fullfile(allResultsFolder, sprintf('%s_%s.csv', allTestFiles(data_idx), datestr(now,'mm-dd-yyyy_HH-MM')));
         scoreTable_tmp = array2table(scoreMatrix_tmp{data_idx, 1});
         scoreTable = [scoreNames scoreTable_tmp];
         scoreTable.Properties.VariableNames = finalTableVariableNames;
-        writetable(scoreTable, all_results_filename);
+        writetable(scoreTable, allResultsFileName);
     end
     
     fileName_Avg = fullfile(outputFolder, sprintf('Average_Scores__%s.csv', datestr(now,'mm-dd-yyyy_HH-MM')));    
--- src/auto_run/trainAndEvaluateAllModels.m
+++ src/auto_run/trainAndEvaluateAllModels.m
@@ -9,33 +9,81 @@ function [finalScores, filesTest] = trainAndEvaluateAllModels(datasetPath, model
 
 fprintf('\nLoading data\n\n')
 % Loading data
-[rawDataTrain, ~, labelsTrain, ~, ...
-    rawDataTest, ~, labelsTest, filesTest, ~] = loadCustomDataset(datasetPath);
+[dataTrainRaw, ~, labelsTrain, ~, ...
+    dataTestRaw, ~, labelsTest, filesTest, ~] = loadCustomDataset(datasetPath);
 
-if isempty(rawDataTest) && isempty(rawDataTrain)
+if isempty(dataTestRaw) && isempty(dataTrainRaw)
     error("Invalid dataset selected");
 end
 
 fprintf('\nPreprocessing data with method: %s\n', preprocMethod);
 % Preprocessing
-[dataTrain, dataTest, ~] = preprocessData(rawDataTrain, ...
-                                            rawDataTest, ...
+[dataTrain, dataTest, ~] = preprocessData(dataTrainRaw, ...
+                                            dataTestRaw, ...
                                             preprocMethod, ...
                                             false, ...
                                             []);
 
-if augmentationEnabled
-    % Augmentation
-    [dataTrain, dataTest] = augmentData(dataTrain, dataTest, augmentationMode, augmentationIntensity, augmentedTrainingEnabled);
+if ~augmentationEnabled
+    % Splitting test/val set
+    [dataTest, labelsTest, filesTest, ...
+        dataTestVal, labelsTestVal, ~] = splitTestVal(dataTest, labelsTest, filesTest, ratioTestVal);
+    
+    % Train all models
+    trainedModels = trainAllModels(models, dataTrain, labelsTrain, dataTestVal, labelsTestVal, thresholds, trainingPlots, parallelEnabled);
+    
+    % Run detection and get scores
+    finalScores = evaluateAllModels(trainedModels, dataTest, labelsTest, filesTest, thresholds, dynamicThresholdSettings);
+else
+    % Evaluation with data augmentation
+    fprintf("\n\nRunning with data augmentation\n\n");
+
+    % Augment data
+    [dataTrainAugmented, dataTestAugmented] = augmentData(dataTrain, dataTest, augmentationMode, augmentationIntensity, augmentedTrainingEnabled);
+
+    % Splitting test/val set
+    [dataTestAugmented, labelsTestAugmented, filesTestAugmented, ...
+        dataTestValAugmented, labelsTestValAugmented, ~] = splitTestVal(dataTestAugmented, labelsTest, filesTest, ratioTestVal);
+    
+    % Train all models
+    trainedModels = trainAllModels(models, dataTrainAugmented, labelsTrain, dataTestValAugmented, labelsTestValAugmented, thresholds, trainingPlots, parallelEnabled);
+    
+    % Run detection and get scores
+    allScoresWithAugmentation = evaluateAllModels(trainedModels, dataTestAugmented, labelsTestAugmented, filesTestAugmented, thresholds, dynamicThresholdSettings);
+
+
+    % Evaluation without augmentation for comparison
+    fprintf("\n\nRunning without data augmentation\n\n");
+
+    % Splitting test/val set
+    [dataTest, labelsTest, filesTest, ...
+        dataTestVal, labelsTestVal, ~] = splitTestVal(dataTest, labelsTest, filesTest, ratioTestVal);
+    
+    % Train all models
+    trainedModels = trainAllModels(models, dataTrain, labelsTrain, dataTestVal, labelsTestVal, thresholds, trainingPlots, parallelEnabled);
+    
+    % Run detection and get scores
+    allScoresWithoutAugmentation = evaluateAllModels(trainedModels, dataTest, labelsTest, filesTest, thresholds, dynamicThresholdSettings);
+
+
+    % Reorder scores
+    numTestedFiles = numel(filesTest);
+    numModels = numel(fieldnames(trainedModels));
+    finalScores = cell(numel(thresholds), 1);
+
+    for thr_idx = 1:numel(thresholds)
+        scores = allScoresWithoutAugmentation{thr_idx, 1};
+        scoresAugmented = allScoresWithAugmentation{thr_idx, 1};
+        scoresMerged = cell(numTestedFiles, 1);
+    
+        for data_idx = 1:numTestedFiles
+            for model_idx = 1:numModels
+                scoresMerged{data_idx, 1} = [scoresMerged{data_idx, 1}, ...
+                    scores{data_idx, 1}(:, model_idx), ...
+                    scoresAugmented{data_idx, 1}(:, model_idx)];
+            end
+        end
+        finalScores{thr_idx, 1} = [finalScores{thr_idx, 1}; scoresMerged];
+    end
 end
-
-% Splitting test/val set
-[dataTest, labelsTest, filesTest, ...
-    dataTestVal, labelsTestVal, ~] = splitTestVal(dataTest, labelsTest, filesTest, ratioTestVal);
-
-% Train all models
-trainedModels = trainAllModels(models, dataTrain, labelsTrain, dataTestVal, labelsTestVal, thresholds, trainingPlots, parallelEnabled);
-
-% Run detection and get scores
-finalScores = evaluateAllModels(trainedModels, dataTest, labelsTest, filesTest, thresholds, dynamicThresholdSettings);
 end
