5c626bf650c0d64b7eb9080b87fbe220ce7ea95e
Adrian Wolf <adrian-wolf1@t-online.de>
2023-04-29 14:39:05 +0200

    update training function

--- src/models_and_training/trainModel.m
+++ src/models_and_training/trainModel.m
@@ -6,52 +6,83 @@ trainedModel.modelOptions = modelOptions;
 
 fprintf("Training: %s ...\n", modelOptions.label);
 
-if ~strcmp(modelOptions.learningType, "unsupervised")
-    if isempty(dataTrain)
-        error("The %s model requires prior training, but the dataset doesn't contain training data (train folder).", modelOptions.label);
-    end
-    
-    % Save dimensionality of data
-    trainedModel.dimensionality = size(dataTrain{1}, 2);
-    
-    % Train model
-    switch modelOptions.type
-        case "deep-learning"
-            [XTrain, YTrain, XVal, YVal] = prepareDataTrain(modelOptions, dataTrain, labelsTrain);
-            [trainedModel.Mdl, trainedModel.MdlInfo] = trainWrapper_DL(modelOptions, XTrain, YTrain, XVal, YVal, trainingPlots, verbose);
-        otherwise
-            [XTrain, YTrain] = prepareDataTrain(modelOptions, dataTrain, labelsTrain);
-            trainedModel.Mdl = trainWrapper_Other(modelOptions, XTrain, YTrain);
-    end
-    
-    % Get static thresholds
-    if ~modelOptions.outputsLabels
-        XTrainTestCell = cell(numel(dataTrain), 1);
-        YTrainTestCell = cell(numel(dataTrain), 1);
-        labelsTrainTest = [];
-    
-        for data_idx = 1:numel(dataTrain)
-            [XTrainTestCell{data_idx}, YTrainTestCell{data_idx}, labelsTrainTest_tmp] = prepareDataTest(modelOptions, dataTrain(data_idx), labelsTrain(data_idx));
-            labelsTrainTest = [labelsTrainTest; labelsTrainTest_tmp];
+switch modelOptions.learningType
+    case "unsupervised"
+        % Do nothing as they don't need to be trained at this point
+    case "semi-supervised"
+        if isempty(dataTrain)
+            error("The %s model requires prior training, but the dataset doesn't contain training data (train folder).", modelOptions.label);
         end
         
-        % Store raw training errors and training labels in trainedModel. Required for some
-        % static thresholds and scoring functions later on.
-        trainedModel.trainingLabels = labelsTrainTest;
-        [trainedModel.trainingAnomalyScoresRaw, trainedModel.trainingAnomalyScoreFeatures] = getTrainingAnomalyScoreFeatures(trainedModel, XTrainTestCell, YTrainTestCell);
+        % Save dimensionality of data
+        trainedModel.dimensionality = size(dataTrain{1}, 2);
         
-        switch modelOptions.learningType
-            case "semi-supervised"
-                % Calc thresholds on anomalous validation set for
-                % semi-supervised models
-                trainedModel.staticThresholds = getStaticThresholds(trainedModel, dataTestVal, labelsTestVal, thresholds, "anomalous-validation-data");
-            case "supervised"
-                % Calc thresholds on training data for supervised models.
-                % (The raw training errors are already stored in the
-                % trainedModel.)
-                trainedModel.staticThresholds = getStaticThresholds(trainedModel, [], [], thresholds, "training-data");
+        % Train model
+        switch modelOptions.type
+            case "deep-learning"
+                [XTrain, YTrain, XVal, YVal] = prepareDataTrain(modelOptions, dataTrain, labelsTrain);
+                [trainedModel.Mdl, trainedModel.MdlInfo] = trainWrapper_DL(modelOptions, XTrain, YTrain, XVal, YVal, trainingPlots, verbose);
+            otherwise
+                [XTrain, YTrain] = prepareDataTrain(modelOptions, dataTrain, labelsTrain);
+                trainedModel.Mdl = trainWrapper_Other(modelOptions, XTrain, YTrain);
         end
-    end
+        
+        % Get static thresholds
+        if ~modelOptions.outputsLabels
+            XTrainTestCell = cell(numel(dataTrain), 1);
+            YTrainTestCell = cell(numel(dataTrain), 1);
+            labelsTrainTest = [];
+        
+            for data_idx = 1:numel(dataTrain)
+                [XTrainTestCell{data_idx}, YTrainTestCell{data_idx}, labelsTrainTest_tmp] = prepareDataTest(modelOptions, dataTrain(data_idx), labelsTrain(data_idx));
+                labelsTrainTest = [labelsTrainTest; labelsTrainTest_tmp];
+            end
+            
+            % Store raw training errors and training labels in trainedModel. Required for some
+            % static thresholds and scoring functions later on.
+            trainedModel.trainingLabels = labelsTrainTest;
+            [trainedModel.trainingAnomalyScoresRaw, trainedModel.trainingAnomalyScoreFeatures] = getTrainingAnomalyScoreFeatures(trainedModel, XTrainTestCell, YTrainTestCell);
+            
+            % Calc thresholds on anomalous validation set
+            trainedModel.staticThresholds = getStaticThresholds(trainedModel, dataTestVal, labelsTestVal, thresholds, "anomalous-validation-data");
+        end
+    case "supervised"
+        if isempty(dataTrain)
+            error("The %s model requires prior training, but the dataset doesn't contain training data (train folder).", modelOptions.label);
+        end
+        
+        % Save dimensionality of data
+        trainedModel.dimensionality = size(dataTrain{1}, 2);
+        
+        % Train model
+        switch modelOptions.type
+            case "deep-learning"
+                [XTrain, YTrain, XVal, YVal] = prepareDataTrain(modelOptions, dataTrain, labelsTrain);
+                [trainedModel.Mdl, trainedModel.MdlInfo] = trainWrapper_DL(modelOptions, XTrain, YTrain, XVal, YVal, trainingPlots, verbose);
+            otherwise
+                [XTrain, YTrain] = prepareDataTrain(modelOptions, dataTrain, labelsTrain);
+                trainedModel.Mdl = trainWrapper_Other(modelOptions, XTrain, YTrain);
+        end
+        
+        % Get static thresholds
+        if ~modelOptions.outputsLabels
+            XTrainTestCell = cell(numel(dataTrain), 1);
+            YTrainTestCell = cell(numel(dataTrain), 1);
+            labelsTrainTest = [];
+        
+            for data_idx = 1:numel(dataTrain)
+                [XTrainTestCell{data_idx}, YTrainTestCell{data_idx}, labelsTrainTest_tmp] = prepareDataTest(modelOptions, dataTrain(data_idx), labelsTrain(data_idx));
+                labelsTrainTest = [labelsTrainTest; labelsTrainTest_tmp];
+            end
+            
+            % Store raw training errors and training labels in trainedModel. Required for some
+            % static thresholds and scoring functions later on.
+            trainedModel.trainingLabels = labelsTrainTest;
+            [trainedModel.trainingAnomalyScoresRaw, trainedModel.trainingAnomalyScoreFeatures] = getTrainingAnomalyScoreFeatures(trainedModel, XTrainTestCell, YTrainTestCell);
+            
+            % Calc thresholds on training data
+            trainedModel.staticThresholds = getStaticThresholds(trainedModel, [], [], thresholds, "training-data");
+        end
 end
 
 fprintf("Successfully trained: %s!\n", modelOptions.label);
